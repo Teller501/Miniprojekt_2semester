47f5d117ee231ad1a993a9c07d8c69869824b1fc
Teller501 <113039777+Teller501@users.noreply.github.com>
2023-03-27 09:55:10 +0200

    Add files via upload

---
+++ src/main/java/dk/kea/Wishlist/WishlistApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.Wishlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dk/kea/Wishlist/WishlistApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.Wishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
