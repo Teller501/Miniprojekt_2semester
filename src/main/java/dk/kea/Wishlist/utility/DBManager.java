a6becc0674ed56056845e090fd40dc589103ba68
Teller501 <ande777j@stud.kea.dk>
2023-04-11 09:50:58 +0200

    modified application.properties

--- src/main/java/dk/kea/Wishlist/utility/DBManager.java
+++ src/main/java/dk/kea/Wishlist/utility/DBManager.java
@@ -1,32 +1,16 @@
 package dk.kea.Wishlist.utility;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class DBManager {
     private static Connection connection = null;
 
     public static Connection getConnection(){
-
-
-        String username = null;
-        String password = null;
-        String url = null;
-
-        try(InputStream input = new FileInputStream("src/main/resources/application.properties")) {
-            Properties properties = new Properties();
-            properties.load(input);
-            url = properties.getProperty("url");
-            username = properties.getProperty("username");
-            password = properties.getProperty("password");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        String url = System.getenv("DATABASE_URL");
+        String username = System.getenv("DATABASE_USERNAME");
+        String password = System.getenv("DATABASE_PASSWORD");
 
         try{
             connection = DriverManager.getConnection(url,username,password);
