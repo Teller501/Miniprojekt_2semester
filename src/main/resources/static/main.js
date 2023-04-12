ed6c3f5b8799a702e07d09d3c36c07f7ed61c050
nicolaiandersson <nico602d@stud.kea.dk>
2023-04-12 23:07:13 +0200

    js stuff so user can sort viewwishlist items by price

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -2,3 +2,22 @@ function goBack() {
     window.history.back();
 }
 
+function sortByPriceAsc() {
+    const wishlist = document.querySelectorAll("tr");
+    Array.from(wishlist).slice(1).sort((a, b) => {
+        const priceA = parseFloat(a.children[2].textContent);
+        const priceB = parseFloat(b.children[2].textContent);
+        return priceA - priceB;
+    }).forEach(tr => wishlist[0].parentNode.appendChild(tr));
+}
+
+function sortByPriceDesc() {
+    const wishlist = document.querySelectorAll("tr");
+    Array.from(wishlist).slice(1).sort((a, b) => {
+        const priceA = parseFloat(a.children[2].textContent);
+        const priceB = parseFloat(b.children[2].textContent);
+        return priceB - priceA;
+    }).forEach(tr => wishlist[0].parentNode.appendChild(tr));
+}
+
+
