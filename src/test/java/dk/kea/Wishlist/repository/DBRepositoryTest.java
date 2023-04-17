b959226ab9ac72f08bee2cfc862381e44ebc8200
Teller501 <ande777j@stud.kea.dk>
2023-04-17 10:00:57 +0200

    added structure for tests

---
+++ src/test/java/dk/kea/Wishlist/repository/DBRepositoryTest.java
@@ -0,0 +1,31 @@
+package dk.kea.Wishlist.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DBRepositoryTest {
+
+    // create some unit tests for DBRepository
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void createWishlist() {
+    }
+
+    @Test
+    void createWish() {
+    }
+}
